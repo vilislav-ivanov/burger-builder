ee45d22c4d5853acf78284f8e5c49ffd669701a5
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-02-16 22:15:52 +0200

    added order for refactor

---
+++ orders/order.js
@@ -0,0 +1,82 @@
+const { InvalidPropertyError } = require('../helpers/errors');
+const requiredParams = require('../helpers/required-params');
+const isNumeric = require('../helpers/is-numeric');
+const isValidEmail = require('../helpers/is-valid-email');
+const upperFirst = require('../helpers/upper-first');
+
+module.exports = function makeOrder(orderInfo = requiredParams('orderInfo')) {
+  const validOrder = validate(orderInfo);
+  const normalOrder = normalize(validOrder);
+  return Object.freeze(normalOrder);
+
+  function validate({
+    price = requiredParams('price'),
+    firstName = requiredParams('firstName'),
+    lastName = requiredParams('lastName'),
+    email = requiredParams('email'),
+    address = requiredParams('address'),
+    postalCode = requiredParams('postalCode'),
+    meat = requiredParams('meat'),
+    bacon = requiredParams('bacon'),
+    salad = requiredParams('salad'),
+    cheese = requiredParams('cheese'),
+    ...otherInfo
+  }) {
+    validateName('firstNmae', firstName);
+    validateName('lastName', lastName);
+    validateEmail(email);
+    validateNumeric('postalCode', postalCode);
+    validateNumeric('price', price);
+    validateNumeric('meat', meat);
+    validateNumeric('bacon', bacon);
+    validateNumeric('salad', salad);
+    validateNumeric('cheese', cheese);
+    validateAddress(address);
+    return {
+      price,
+      firstName,
+      lastName,
+      email,
+      address,
+      postalCode,
+      meat,
+      bacon,
+      salad,
+      heese,
+      ...otherInfo,
+    };
+  }
+  function normalize({ firstName, lastName, email, ...orderInfo }) {
+    return {
+      ...orderInfo,
+      firstName: upperFirst(firstName),
+      lastName: upperFirst(lastName),
+      email: email.toLowerCase(),
+    };
+  }
+
+  function validateName(label, name) {
+    if (name.length < 2) {
+      throw new InvalidPropertyError(
+        `${label} name must be at least 2 symbols long.`
+      );
+    }
+  }
+  function validateNumeric(label, num) {
+    if (!isNumeric(num)) {
+      throw new InvalidPropertyError(`${label} must be a number value.`);
+    }
+  }
+  function validateEmail(email) {
+    if (!isValidEmail(email)) {
+      throw new InvalidPropertyError(`${email} is not valid email address.`);
+    }
+  }
+  function validateAddress(address) {
+    if (address.length < 3) {
+      throw new InvalidPropertyError(
+        'Address must be at least 3 symbols long.'
+      );
+    }
+  }
+};
