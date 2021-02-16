8350830b5f3daaaee573ebf63712dce22d662e94
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-02-16 22:17:18 +0200

    added mongodb, remove mongoose and setup db

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const mongodb = require('mongodb');
+const { mongoUrl, dbName } = require('../config/keys');
+
+function makeIdFromString(id) {
+  return new mongodb.ObjectID(id);
+}
+
+module.exports = function () {
+  const MongoClient = mongodb.MongoClient;
+  const client = new MongoClient(mongoUrl, { useNewUrlParser: true });
+  client.connect();
+  const db = client.db(dbName);
+  db.makeId = makeIdFromString;
+  return db;
+};
